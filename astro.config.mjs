d2eeecbdeb268afd61c68f382e0b61aeb88a7dd2
shabranali786 <shabranoffice786@gmail.com>
2025-10-17 00:54:57 +0500

    1. google font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  experimental: {
+    fonts: [
+      // Poppins Font Configuration
+      {
+        provider: fontProviders.google(),
+        name: "Poppins",
+        cssVariable: "--font-poppins",
+        weights: [100, 200, 400, 500, 600, 700, 800],
+        styles: ["normal"],
+        subsets: ["latin"],
+        display: "swap",
+        fallbacks: ["sans-serif"],
+      },
+
+      // Roboto Font Configuration
+      {
+        provider: fontProviders.google(),
+        name: "Roboto",
+        cssVariable: "--font-roboto",
+        weights: [400, 500, 700],
+        styles: ["normal", "italic"],
+        subsets: ["latin"],
+        display: "swap",
+        fallbacks: ["sans-serif"],
+      },
+    ],
+  },
 });
